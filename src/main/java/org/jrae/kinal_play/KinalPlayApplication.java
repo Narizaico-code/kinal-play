df43bb2c1f15caac30b785606229da3c2b1749a2
Informatica <informatica@alumno.local>
2025-09-04 15:55:01 -0600

    primer commit

---
+++ src/main/java/org/jrae/kinal_play/KinalPlayApplication.java
@@ -0,0 +1,13 @@
+package org.jrae.kinal_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinalPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinalPlayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/jrae/kinal_play/web/controller/HolaController.java
@@ -0,0 +1,13 @@
+package org.jrae.kinal_play.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        return "Hola mundo estoy en Spring Boot";
+    }
+}
